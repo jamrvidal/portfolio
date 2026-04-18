202f6f39acbba8d43721db6346aeac4666f98d8a
James Vidal <james.vidal2006@gmail.com>
2026-04-17 23:12:27 -0700

    js integration + light and dark mode

---
+++ global.js
@@ -0,0 +1,79 @@
+console.log('IT’S ALIVE!');
+
+// function $$(selector, context = document) {
+//   return Array.from(context.querySelectorAll(selector));
+// }
+
+// let navLinks = $$("nav a");
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+// );
+
+// currentLink?.classList.add('current')
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"
+  : "/portfolio/"; 
+
+let pages = [
+    {url: '', title: 'Home'},
+    {url: 'projects/', title: 'Projects'},
+    {url: 'contact/', title: 'Contact'},
+    {url: 'resume/', title: 'Resume'},
+    {url: 'https://github.com/jamrvidal', title: 'Github'}
+]
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages){
+    let url = p.url;
+    let title = p.title;
+    
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname ===location.pathname
+    );
+
+    if (a.host !== location.host){
+        a.target = "_blank";
+    }
+    nav.append(a);
+}
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+        <label class='color-scheme'>
+            Theme:
+            <select>
+                <option> light dark </option>
+                <option> light</option>
+                <option> dark</option>
+            </select>
+        </label>`
+);
+
+const select = document.querySelector('.color-scheme select');
+
+select.addEventListener('input', function (event){
+    console.log('color scheme changed to', event.target.value);
+
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+
+    localStorage.colorScheme = event.target.value;
+});
+
+if ('colorScheme in localStorage'){
+    const savedScheme = localStorage.colorScheme;
+
+    document.documentElement.style.setProperty('color-scheme', savedScheme);
+
+    select.value = savedScheme;
+}
