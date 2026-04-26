1e2f890c8b0c049fd10987d4149e4ce0c87c5059
James Vidal <james.vidal2006@gmail.com>
2026-04-25 23:14:58 -0700

    lab 4 changes
    all the changes for lab 4, the projects page is updated as a json

--- global.js
+++ global.js
@@ -9,7 +9,6 @@ console.log('IT’S ALIVE!');
 //     (a) => a.host === location.host && a.pathname === location.pathname
 // );
 
-// currentLink?.classList.add('current')
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"
@@ -76,4 +75,37 @@ if ('colorScheme' in localStorage){
     document.documentElement.style.setProperty('color-scheme', savedScheme);
 
     select.value = savedScheme;
+}
+
+
+export async function fetchJSON(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch: ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching or parsing JSON data:', error);
+    }
+}
+
+export async function fetchGitHubData(username) {
+    return fetchJSON(`https://api.github.com/users/${username}`);
+}
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+    containerElement.innerHTML = '';
+
+     projects.forEach(project => {
+        const article = document.createElement('article');
+        article.innerHTML = `
+            <${headingLevel}>${project.title}</${headingLevel}>
+            <img src="${project.image}" alt="${project.title}">
+            <p>${project.description}</p>
+            <footer>c. ${project.year}</footer>
+        `;
+        containerElement.appendChild(article);
+    });
 }
